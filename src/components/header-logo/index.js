75bb1569ec0b48a98754bf5f412e4a1f7be5fee9
madebysi <dev.madebysi@gmail.com>
2018-04-16 20:14:37 +0100

    :art: SVG logo

--- src/components/header-logo/index.js
+++ src/components/header-logo/index.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import styled from 'styled-components';
-import {colors, media} from '../../styles';
+import logoBlack from './logo-black.svg';
+import logoWhite from './logo-white.svg';
 
-const Logo = styled.span`
-    color: ${({dark}) => dark ? colors.black : colors.white};
-    transition: color 0.4s ease;
-    font-size: 21px;
-    font-weight: bold;
+const Img = styled.img`
+    display: block;
+    height: 23.52px;
     position: relative;
     z-index: 1;
-
-    ${media.md`
-        font-size: 24px;
-    `}
 `;
 
 export default ({to, dark}) => (
     <Link to={to}>
-        <Logo dark={dark}>madebySi</Logo>
+        <Img src={dark ? logoBlack : logoWhite} alt="madebySi" />
     </Link>
 );
